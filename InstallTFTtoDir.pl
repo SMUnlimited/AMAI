06468b1650f8e21c3740bbad2969b7d0f8936d2e
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2022-11-06 00:24:47 +0800

    sync infinite loop in perl script fixed

--- InstallTFTtoDir.pl
+++ InstallTFTtoDir.pl
@@ -4,6 +4,7 @@ use strict;
 
 sub process_dir {
 	my $dirname = $_[0];
+  my $commander = $_[1];
 	my $filename;
 	
 	opendir my $DIR, $dirname or die "Could not open $dirname\n";
@@ -12,17 +13,23 @@ sub process_dir {
 		next if $filename eq "." || $filename eq "..";
 		if ((-d "$dirname/$filename") ) {
 			print "Installing AMAI to dir $dirname\\$filename\n";
-			process_dir ("$dirname\\$filename");
-		} elsif ($filename =~ m/\.w3m$/ || $filename =~ m/\.w3x$/ ) { 
-			print "Installing AMAI to $dirname/$filename\n";
+			process_dir ("$dirname\\$filename", $commander);
+		} elsif ($filename =~ m/\.w3m$/ || $filename =~ m/\.w3x$/ ) {
+      if (!(defined($commander)) || $commander eq "true") {
+        print "Installing AMAI and Commander to $dirname/$filename\n";
+      } else {
+        print "Installing AMAI without Commander to $dirname/$filename\n";
+      }
 			system "MPQEditor htsize \"$dirname/$filename\" 64";
-			system "AddToMPQ.exe \"$dirname/$filename\" Scripts\\TFT\\common.ai Scripts\\common.ai Scripts\\Blizzard.j Scripts\\Blizzard.j";
-			system "AddToMPQ.exe \"$dirname/$filename\" Scripts\\TFT\\elf.ai Scripts\\elf.ai Scripts\\TFT\\human.ai Scripts\\human.ai Scripts\\TFT\\orc.ai Scripts\\orc.ai Scripts\\TFT\\undead.ai Scripts\\undead.ai";
-			system "AddToMPQ.exe \"$dirname/$filename\" Scripts\\TFT\\elf2.ai Scripts\\elf2.ai Scripts\\TFT\\human2.ai Scripts\\human2.ai Scripts\\TFT\\orc2.ai Scripts\\orc2.ai Scripts\\TFT\\undead2.ai Scripts\\undead2.ai";
+			system "MPQEditor a \"$dirname/$filename\" Scripts\\TFT\\*.ai Scripts";
+      if (!(defined($commander)) || $commander eq "true") {
+        system "MPQEditor a \"$dirname/$filename\" Scripts\\Blizzard.j Scripts\\Blizzard.j";
+      }
+			system "MPQEditor f \"$dirname/$filename\""
 		}
 	}
 
 	closedir($DIR);
 }
 
-process_dir ($ARGV[0]);
+process_dir ($ARGV[0], $ARGV[1]);
