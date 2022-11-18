097e10cccc64a81d95a35b395a24a65e5f75b65a
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2022-11-19 00:14:08 +0800

    build RocBace.bat

--- MakeROC.bat
+++ MakeROC.bat
@@ -1,36 +1,5 @@
 @ECHO OFF
-ECHO Making AMAI
-ECHO _____________________________
-ECHO creating \Scripts\ROC\common.ai
-perl ejass.pl common.eai ROC VER:ROC > Scripts\ROC\common.ai
-ECHO \Scripts\ROC\common.ai created
-pjass common.j Scripts\ROC\common.ai
-ECHO _____________________________
-ECHO creating \Scripts\ROC\elf.ai
-perl ejass.pl races.eai ROC VER:ROC ELF RACE:ELF > Scripts\ROC\elf.ai
-ECHO \Scripts\ROC\elf.ai created
-pjass common.j Scripts\ROC\common.ai Scripts\ROC\elf.ai
-ECHO _____________________________
-ECHO creating \Scripts\ROC\human.ai
-perl ejass.pl races.eai ROC VER:ROC HUMAN RACE:HUMAN > Scripts\ROC\human.ai
-ECHO \Scripts\ROC\human.ai created
-pjass common.j Scripts\ROC\common.ai Scripts\ROC\human.ai
-ECHO _____________________________
-ECHO creating \Scripts\ROC\orc.ai
-perl ejass.pl races.eai ROC VER:ROC ORC RACE:ORC > Scripts\ROC\orc.ai
-ECHO \Scripts\ROC\orc.ai created
-pjass common.j Scripts\ROC\common.ai Scripts\ROC\orc.ai
-ECHO _____________________________
-ECHO creating \Scripts\ROC\undead.ai
-perl ejass.pl races.eai ROC VER:ROC UNDEAD RACE:UNDEAD > Scripts\ROC\undead.ai
-ECHO \Scripts\ROC\undead.ai created
-pjass common.j Scripts\ROC\common.ai Scripts\ROC\undead.ai
-ECHO _____________________________
-ECHO creating \Scripts\Blizzard.j
-perl ejass.pl Blizzard.eai ROC VER:ROC > Scripts\Blizzard.j
-ECHO \Scripts\Blizzard.j created
-pjass common.j Scripts\Blizzard.j
-ECHO _____________________________
+call MakeROCBase.bat 0
 ECHO =============================
 ECHO Making AMAI finished
 pause
