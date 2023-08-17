194175495876d78e6febb97a5b8c7acb859cba33
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2023-08-17 10:04:52 +0800

    FIX

--- MakeROCBase.bat
+++ MakeROCBase.bat
@@ -12,25 +12,25 @@ ECHO creating \Scripts\ROC\elf.ai
 perl ejass.pl races.eai ROC VER:ROC ELF RACE:ELF > Scripts\ROC\elf.ai
 ECHO \Scripts\ROC\elf.ai created
 pjass common.j Scripts\ROC\common.ai Scripts\ROC\elf.ai
-jassparser common.j Scripts\TFT\common.ai Scripts\ROC\elf.ai
+jassparser common.j Scripts\ROC\common.ai Scripts\ROC\elf.ai
 ECHO _____________________________
 ECHO creating \Scripts\ROC\human.ai
 perl ejass.pl races.eai ROC VER:ROC HUMAN RACE:HUMAN > Scripts\ROC\human.ai
 ECHO \Scripts\ROC\human.ai created
 pjass common.j Scripts\ROC\common.ai Scripts\ROC\human.ai
-jassparser common.j Scripts\TFT\common.ai Scripts\ROC\human.ai
+jassparser common.j Scripts\ROC\common.ai Scripts\ROC\human.ai
 ECHO _____________________________
 ECHO creating \Scripts\ROC\orc.ai
 perl ejass.pl races.eai ROC VER:ROC ORC RACE:ORC > Scripts\ROC\orc.ai
 ECHO \Scripts\ROC\orc.ai created
 pjass common.j Scripts\ROC\common.ai Scripts\ROC\orc.ai
-jassparser common.j Scripts\TFT\common.ai Scripts\ROC\orc.ai
+jassparser common.j Scripts\ROC\common.ai Scripts\ROC\orc.ai
 ECHO _____________________________
 ECHO creating \Scripts\ROC\undead.ai
 perl ejass.pl races.eai ROC VER:ROC UNDEAD RACE:UNDEAD > Scripts\ROC\undead.ai
 ECHO \Scripts\ROC\undead.ai created
 pjass common.j Scripts\ROC\common.ai Scripts\ROC\undead.ai
-jassparser common.j Scripts\TFT\common.ai Scripts\ROC\undead.ai
+jassparser common.j Scripts\ROC\common.ai Scripts\ROC\undead.ai
 ECHO _____________________________
 ECHO creating \Scripts\Blizzard.j VSAI Flag set to %VSAI%
 perl SplitBlizzardJ.pl
