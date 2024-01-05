9a932d0e27e829d4be32c2a375f910ccf36d3855
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-05 12:30:22 +0800

    fix

--- MakeOptTFT.bat
+++ MakeOptTFT.bat
@@ -1,45 +1,44 @@
 @ECHO OFF
 SET LOG=%~1
-SET VER=TFT
 if not "%LOG%"=="0" SET LOG=1
 ECHO Synchronize files
-perl ejass.pl REFORGED\AddedAggression.txt > %VER%\AddedAggression.txt
-perl ejass.pl REFORGED\CommandBuilds.txt > %VER%\CommandBuilds.txt
-perl ejass.pl REFORGED\Dragons.txt > %VER%\Dragons.txt
-perl ejass.pl REFORGED\DragonTypes.txt > %VER%\DragonTypes.txt
-perl ejass.pl REFORGED\Healers.txt > %VER%\Healers.txt
-perl ejass.pl REFORGED\HealingItems.txt > %VER%\HealingItems.txt
-perl ejass.pl REFORGED\HeroAbilities.txt > %VER%\HeroAbilities.txt
-perl ejass.pl REFORGED\HeroLevels.txt > %VER%\HeroLevels.txt
-perl ejass.pl REFORGED\ItemCheck.txt > %VER%\ItemCheck.txt
-perl ejass.pl REFORGED\MercTypes.txt > %VER%\MercTypes.txt
-perl ejass.pl REFORGED\NeutralShops.txt > %VER%\NeutralShops.txt
-perl ejass.pl REFORGED\Profiles.txt > %VER%\Profiles.txt
-perl ejass.pl REFORGED\Races.txt > %VER%\Races.txt
-perl ejass.pl REFORGED\StandardAiSettings.txt > %VER%\StandardAiSettings.txt
-perl ejass.pl REFORGED\Strengths.txt > %VER%\Strengths.txt
-perl ejass.pl REFORGED\Upkeep.txt > %VER%\Upkeep.txt
-perl ejass.pl REFORGED\UnitConversions.txt > %VER%\UnitConversions.txt
+perl ejass.pl REFORGED\AddedAggression.txt > TFT\AddedAggression.txt
+perl ejass.pl REFORGED\CommandBuilds.txt > TFT\CommandBuilds.txt
+perl ejass.pl REFORGED\Dragons.txt > TFT\Dragons.txt
+perl ejass.pl REFORGED\DragonTypes.txt > TFT\DragonTypes.txt
+perl ejass.pl REFORGED\Healers.txt > TFT\Healers.txt
+perl ejass.pl REFORGED\HealingItems.txt > TFT\HealingItems.txt
+perl ejass.pl REFORGED\HeroAbilities.txt > TFT\HeroAbilities.txt
+perl ejass.pl REFORGED\HeroLevels.txt > TFT\HeroLevels.txt
+perl ejass.pl REFORGED\ItemCheck.txt > TFT\ItemCheck.txt
+perl ejass.pl REFORGED\MercTypes.txt > TFT\MercTypes.txt
+perl ejass.pl REFORGED\NeutralShops.txt > TFT\NeutralShops.txt
+perl ejass.pl REFORGED\Profiles.txt > TFT\Profiles.txt
+perl ejass.pl REFORGED\Races.txt > TFT\Races.txt
+perl ejass.pl REFORGED\StandardAiSettings.txt > TFT\StandardAiSettings.txt
+perl ejass.pl REFORGED\Strengths.txt > TFT\Strengths.txt
+perl ejass.pl REFORGED\Upkeep.txt > TFT\Upkeep.txt
+perl ejass.pl REFORGED\UnitConversions.txt > TFT\UnitConversions.txt
 
-perl ejass.pl REFORGED\Elf\CommandBuilds.txt > %VER%\Elf\CommandBuilds.txt
-perl ejass.pl REFORGED\Elf\RaceAggression.txt > %VER%\Elf\RaceAggression.txt
-perl ejass.pl REFORGED\Elf\Settings.txt > %VER%\Elf\Settings.txt
-perl ejass.pl REFORGED\Elf\Tiers.txt > %VER%\Elf\Tiers.txt
+perl ejass.pl REFORGED\Elf\CommandBuilds.txt > TFT\Elf\CommandBuilds.txt
+perl ejass.pl REFORGED\Elf\RaceAggression.txt > TFT\Elf\RaceAggression.txt
+perl ejass.pl REFORGED\Elf\Settings.txt > TFT\Elf\Settings.txt
+perl ejass.pl REFORGED\Elf\Tiers.txt > TFT\Elf\Tiers.txt
 
-perl ejass.pl REFORGED\Human\CommandBuilds.txt > %VER%\Human\CommandBuilds.txt
-perl ejass.pl REFORGED\Human\RaceAggression.txt > %VER%\Human\RaceAggression.txt
-perl ejass.pl REFORGED\Human\Settings.txt > %VER%\Human\Settings.txt
-perl ejass.pl REFORGED\Human\Tiers.txt > %VER%\Human\Tiers.txt
+perl ejass.pl REFORGED\Human\CommandBuilds.txt > TFT\Human\CommandBuilds.txt
+perl ejass.pl REFORGED\Human\RaceAggression.txt > TFT\Human\RaceAggression.txt
+perl ejass.pl REFORGED\Human\Settings.txt > TFT\Human\Settings.txt
+perl ejass.pl REFORGED\Human\Tiers.txt > TFT\Human\Tiers.txt
 
-perl ejass.pl REFORGED\Orc\CommandBuilds.txt > %VER%\Orc\CommandBuilds.txt
-perl ejass.pl REFORGED\Orc\RaceAggression.txt > %VER%\Orc\RaceAggression.txt
-perl ejass.pl REFORGED\Orc\Settings.txt > %VER%\Orc\Settings.txt
-perl ejass.pl REFORGED\Orc\Tiers.txt > %VER%\Orc\Tiers.txt
+perl ejass.pl REFORGED\Orc\CommandBuilds.txt > TFT\Orc\CommandBuilds.txt
+perl ejass.pl REFORGED\Orc\RaceAggression.txt > TFT\Orc\RaceAggression.txt
+perl ejass.pl REFORGED\Orc\Settings.txt > TFT\Orc\Settings.txt
+perl ejass.pl REFORGED\Orc\Tiers.txt > TFT\Orc\Tiers.txt
 
-perl ejass.pl REFORGED\Undead\CommandBuilds.txt > %VER%\Undead\CommandBuilds.txt
-perl ejass.pl REFORGED\Undead\RaceAggression.txt > %VER%\Undead\RaceAggression.txt
-perl ejass.pl REFORGED\Undead\Settings.txt > %VER%\Undead\Settings.txt
-perl ejass.pl REFORGED\Undead\Tiers.txt > %VER%\Undead\Tiers.txt
+perl ejass.pl REFORGED\Undead\CommandBuilds.txt > TFT\Undead\CommandBuilds.txt
+perl ejass.pl REFORGED\Undead\RaceAggression.txt > TFT\Undead\RaceAggression.txt
+perl ejass.pl REFORGED\Undead\Settings.txt > TFT\Undead\Settings.txt
+perl ejass.pl REFORGED\Undead\Tiers.txt > TFT\Undead\Tiers.txt
 ECHO Synchronize files done
 
 call MakeOptVER TFT %LOG%
