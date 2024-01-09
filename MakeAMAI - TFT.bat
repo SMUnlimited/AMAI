f203eddf4e7c7f6ba7b765127e135c1408e1c2b4
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-09 09:43:23 +0800

    在线编译

---
+++ MakeAMAI - TFT.bat
@@ -0,0 +1,40 @@
+@ECHO OFF
+ECHO Making AMAI
+ECHO _____________________________
+ECHO creating \在线工作流-测试\common.ai
+perl ejass.pl common.eai TFT VER:TFT > 在线工作流-测试\common.ai
+ECHO IGNORE jasshelper --scriptonly common.j Scripts\Blizzard.j 在线工作流-测试\common.ai 在线工作流-测试\common2.ai
+ECHO \在线工作流-测试\common.ai created
+pjass common.j 在线工作流-测试\common.ai
+jassparser common.j 在线工作流-测试\common.ai
+ECHO _____________________________
+ECHO creating \在线工作流-测试\elf.ai
+perl ejass.pl races.eai TFT VER:TFT ELF RACE:ELF > 在线工作流-测试\elf.ai
+ECHO \在线工作流-测试\elf.ai created
+pjass common.j 在线工作流-测试\common.ai 在线工作流-测试\elf.ai
+jassparser common.j 在线工作流-测试\common.ai 在线工作流-测试\elf.ai
+ECHO _____________________________
+ECHO creating \在线工作流-测试\human.ai
+perl ejass.pl races.eai TFT VER:TFT HUMAN RACE:HUMAN > 在线工作流-测试\human.ai
+ECHO \在线工作流-测试\human.ai created
+pjass common.j 在线工作流-测试\common.ai 在线工作流-测试\human.ai
+jassparser common.j 在线工作流-测试\common.ai 在线工作流-测试\human.ai
+ECHO _____________________________
+ECHO creating \在线工作流-测试\orc.ai
+perl ejass.pl races.eai TFT VER:TFT ORC RACE:ORC > 在线工作流-测试\orc.ai
+ECHO \在线工作流-测试\orc.ai created
+pjass common.j 在线工作流-测试\common.ai 在线工作流-测试\orc.ai
+jassparser common.j 在线工作流-测试\common.ai 在线工作流-测试\orc.ai
+ECHO _____________________________
+ECHO creating \在线工作流-测试\undead.ai
+perl ejass.pl races.eai TFT VER:TFT UNDEAD RACE:UNDEAD > 在线工作流-测试\undead.ai
+ECHO \在线工作流-测试\undead.ai created
+pjass common.j 在线工作流-测试\common.ai 在线工作流-测试\undead.ai
+jassparser common.j 在线工作流-测试\common.ai 在线工作流-测试\undead.ai
+ECHO _____________________________
+ECHO creating \在线工作流-测试\Blizzard.j
+perl ejass.pl Blizzard.eai TFT VER:TFT > 在线工作流-测试\Blizzard.j
+ECHO \在线工作流-测试\Blizzard.j created
+pjass common.j 在线工作流-测试\Blizzard.j
+jassparser common.j 在线工作流-测试\Blizzard.j
+ECHO _____________________________
