4eba9397fef41a916a8e1c6df008cc8bb7a90783
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-06 17:28:23 +0800

    Add files via upload

--- Electron/commons/models/index.js
+++ Electron/commons/models/index.js
@@ -3,5 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.InstallModel = void 0;
 class InstallModel {
 }
-exports.InstallModel = InstallModel;
 //# sourceMappingURL=index.js.map
