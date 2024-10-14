e502686421bcc869e963020791bd37d2762fc4d6
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-10-14 20:47:51 +0800

    同步
    【同步官方】修复第二靠近的盟友在自己主英雄不可用时距离计算错误
    【同步官方】疯狂电脑将精确计算敌人力量
    【同步官方】调整非中文聊天
    【同步官方】调整睡眠逻辑以减少崩溃
    【同步官方】支持LUA地图
    【同步官方】经典版冰封王座不再复制重制版文件
    修复战斗休眠

--- MakeTFT.bat
+++ MakeTFT.bat
@@ -3,9 +3,9 @@ SET MAKEALL=%~1
 if not "%MAKEALL%"=="1" (
   SET MAKEALL=0
 )
-call TFT/Inherit.bat
+@REM call TFT/Inherit.bat Manually call this when need to refresh TFT state.
 ECHO _____________________________
-call MakeVERBase.bat 0 TFT %MAKEALL%
+call MakeVERBase.bat TFT %MAKEALL%
 ECHO _____________________________
 call ForwardsCompat.bat TFT
 ECHO =============================
