0d871fd7e7f298f428e8f522d735a05a2b5d735f
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-10-22 22:03:20 +0800

    Update InstallAppointVERToMap.bat

--- InstallAppointVERToMap.bat
+++ InstallAppointVERToMap.bat
@@ -7,12 +7,12 @@ SET COMMAND=1
 cls
 echo.
 echo Please select war3 version:
-echo 1. REFORGED(1.33+)(default)
-echo 2. TFT(1.24e~1.32.10)
-echo 3. ROC(1.24e~1.31)
+echo 1. REFORGED (1.33+) (default)
+echo 2. TFT (1.24e ~ 1.32.10)
+echo 3. ROC (1.24e ~ 1.31)
 
 echo.
-set /p choice=Input(1-3):
+set /p choice=Input(1 ~ 3):
 
 if "%choice%"=="1" (
   set VER=REFORGED
@@ -30,11 +30,11 @@ goto comMenu
 cls
 echo.
 echo Installation Commander?:
-echo 1. Install Commander(default)
+echo 1. Install Commander (default)
 echo 2. Install VS AI Commander
 echo 3. Not Install Commander
 echo.
-set /p choice=Input(1-3):
+set /p choice=Input(1 ~ 3):
 
 if "%choice%"=="1" (
   SET COMMAND=0
@@ -46,12 +46,24 @@ if "%choice%"=="3" (
   SET COMMAND=2
 )
 
-goto InputPath
+goto InstallMenu
 
+:InstallMenu
+cls
+echo.
+echo Please choose the installation method:
+Echo. 1. Batch installation (default)
+Echo. 2. Install a single map
+echo.
+set /p choice=Input(1 ~ 2):
+
+if "%choice%"=="1" (
 :InputPath
 cls
 echo.
-set /p searchPath=Please enter the complete path of the map folder:
+ECHO Please enter the complete path of the map folder.
+ECHO e.g. G:\Documents\Warcraft III\Maps\
+set /p searchPath=Input:
 
 setlocal enabledelayedexpansion
 for %%F in ("%searchPath%\*.w3x" "%searchPath%\*.w3m") do (
@@ -59,3 +71,16 @@ for %%F in ("%searchPath%\*.w3x" "%searchPath%\*.w3m") do (
 )
 endlocal
 pause
+)
+if not "%choice%"=="1" (
+:InputMap
+cls
+echo.
+ECHO Please enter the complete path of the map folder and map File Name (with Format).
+ECHO e.g. G:\Documents\Warcraft III\Maps\Friends_v1.2.w3x
+set /p filePath=Input:
+ECHO %filePath%
+  call InstallVERToMap %VER% "%filePath%" "%COMMAND%"
+pause
+goto InputMap
+)
