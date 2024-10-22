b8eea84fb7e9e51720f313a4eba38f7e06a71d8b
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-10-23 02:16:55 +0800

    Update InstallAppointVERToMap.bat

--- InstallAppointVERToMap.bat
+++ InstallAppointVERToMap.bat
@@ -1,17 +1,18 @@
-@echo off
+@ECHO off
 chcp 65001
 SET VER=REFORGED
 SET COMMAND=1
+SET COMMAND_STATE=Install Commander
 
 :VERMenu
 cls
-echo.
-echo Please select war3 version:
-echo 1. REFORGED (1.33+) (default)
-echo 2. TFT (1.24e ~ 1.32.10)
-echo 3. ROC (1.24e ~ 1.31)
-
-echo.
+ECHO.
+ECHO (1/4) Please select War3 version:
+ECHO.
+ECHO 1. REFORGED (1.33+) (default)
+ECHO 2. TFT (1.24e ~ 1.32.10)
+ECHO 3. ROC (1.24e ~ 1.31)
+ECHO.
 set /p choice=Input(1 ~ 3):
 
 if "%choice%"=="1" (
@@ -28,45 +29,58 @@ goto comMenu
 
 :comMenu
 cls
-echo.
-echo Installation Commander?:
-echo 1. Install Commander (default)
-echo 2. Install VS AI Commander
-echo 3. Not Install Commander
-
-echo.
+ECHO.
+ECHO War3 version: %VER%
+ECHO.
+ECHO (2/4) Installation Commander?:
+ECHO 1. Install Commander (default)
+ECHO 2. Install VS AI Commander
+ECHO 3. Not Install Commander
+ECHO.
 set /p choice=Input(1 ~ 3):
 
 if "%choice%"=="1" (
   SET COMMAND=0
+  SET COMMAND_STATE=Install Commander
 )
 if "%choice%"=="2" (
   SET COMMAND=1
+  SET COMMAND_STATE=Install VS AI Commander
 )
 if "%choice%"=="3" (
   SET COMMAND=2
+  SET COMMAND_STATE=Not Install Commander
 )
 
 goto InstallMenu
 
 :InstallMenu
 cls
-echo.
-echo Please choose the installation method:
-Echo. 1. Batch installation (default)
-Echo. 2. Install a single map
-
-echo.
+ECHO.
+ECHO War3 version: %VER% , Commander: %COMMAND_STATE%
+ECHO.
+ECHO (3/4) Please choose the installation method:
+ECHO. 1. Install to Map Folder (default)
+ECHO. 2. Install to Single Map
+ECHO.
 set /p choice=Input(1 ~ 2):
 
 if "%choice%"=="1" (
+  goto InputPath
+)
+
+if not "%choice%"=="1" (
+  goto InputMap
+)
+
 :InputPath
 cls
-echo.
-ECHO Please enter the complete path of the map folder.
+ECHO.
+ECHO War3 version: %VER% , Commander: %COMMAND_STATE% , Installation Method : Map Folder
+ECHO.
+ECHO (4/4) Please enter the complete path of the map folder.
 ECHO e.g. C:\Documents\Warcraft III\Maps
-
-echo.
+ECHO.
 set /p searchPath=Input:
 
 setlocal enabledelayedexpansion
@@ -74,20 +88,22 @@ for %%F in ("%searchPath%\*.w3x" "%searchPath%\*.w3m") do (
   call InstallVERToMap !VER! "!%%~fF!" "!COMMAND!"
 )
 endlocal
-pause
-)
+goto EndScript
 
-if not "%choice%"=="1" (
 :InputMap
 cls
-echo.
-ECHO Please enter the complete path of the map folder and map File Name (with Format).
+ECHO.
+ECHO War3 version: %VER% , Commander: %COMMAND_STATE% , Installation Method : Single Map
+ECHO.
+ECHO (4/4) Please enter the complete path of the map folder and map File Name (with Format).
 ECHO e.g. C:\Documents\Warcraft III\Maps\Friends_v1.2.w3x
-
-echo.
+ECHO.
 set /p filePath=Input:
 
 call InstallVERToMap %VER% "%filePath%" "%COMMAND%"
+ECHO If the installation is complete, please close this window, else please click any key to continue installation.
 pause
 goto InputMap
-)
+
+:EndScript
+pause
