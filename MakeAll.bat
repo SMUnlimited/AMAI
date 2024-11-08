a10a2b0273ee8801d7dc85de0240c8a0e50d19ef
SMUnlimited <SMUnlimited@users.noreply.github.com>
2024-11-08 18:03:12 +0000

    More github automation fixing

--- MakeAll.bat
+++ MakeAll.bat
@@ -1,29 +1,35 @@
 @ECHO OFF
 @call MakeRoC 1
+set FAILURE=0
 if "%RESULTMAKEVER%"=="1" (
+  set FAILURE=1
   pause
 )
 @call MakeTFT 1
 if "%RESULTMAKEVER%"=="1" (
+  set FAILURE=1
   pause
 )
 @call MakeREFORGED 1
 if "%RESULTMAKEVER%"=="1" (
+  set FAILURE=1
   pause
 )
 @call MakeOptROC 1
 if "%RESULTOPTVER%"=="1" (
+  set FAILURE=1
   pause
 )
 @call MakeOptTFT 1
 if "%RESULTOPTVER%"=="1" (
+  set FAILURE=1
   pause
 )
 @call MakeOptREFORGED 1
 if "%RESULTOPTVER%"=="1" (
+  set FAILURE=1
   pause
-  exit /b %RESULTOPTVER%
 )
-if "%RESULTMAKEVER%"=="1" (
-  exit /b %RESULTMAKEVER%
+if "%FAILURE%"=="1" (
+  exit /b %FAILURE%
 )
