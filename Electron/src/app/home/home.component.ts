f93b9bb698f8b21b573a0c7ffed126d3f85bf255
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2026-02-21 20:02:41 +0800

    Add files via upload

--- Electron/src/app/home/home.component.ts
+++ Electron/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ElectronService } from '../core/services/electron/electron.service';
+import { TranslateService, LangChangeEvent } from "@codeandweb/ngx-translate";
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,75 @@ import { ElectronService } from '../core/services/electron/electron.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  defaultPath: string | null = null;
+  defaultPathText: string = '';
+
+  constructor(private electronService: ElectronService, private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    console.log('HomeComponent INIT');
+    this.loadDefaultPath();
+  }
+
+  loadDefaultPath(): void {
+    if (this.electronService.isElectron) {
+      this.electronService.ipcRenderer.invoke('file-operations', {
+        operation: 'load-default-path'
+      }).then((path: string | null) => {
+        console.log('Loaded default path:', path);
+        this.defaultPath = path;
+        if (path) {
+          this.defaultPathText = this.formatPath(path);
+          this.defaultPath = path;
+        } else {
+          this.defaultPathText = '';
+          this.defaultPath = null;
+        }
+      }).catch(error => {
+        console.error('Error loading default path:', error);
+        this.defaultPathText = this.translate.instant('PAGES.HOME.CAN_NOT_GET_DEFAULT_PATH');
+      });
+    }
+  }
+
+  private formatPath(path: string, maxLength = 30): string {
+    if (path.length <= maxLength) return path;
+    
+    const parts = path.split(/[\\/]/);
+    if (parts.length <= 2) return path;
+    
+    const firstPart = parts[0];
+    const lastPart = parts[parts.length - 1];
+    const middleLength = maxLength - (firstPart.length + lastPart.length + 5);
+    
+    if (middleLength > 0) {
+      return `${firstPart}/.../${lastPart}`;
+    }
+    return `${firstPart}/...${lastPart}`;
+  }
+  async selectDefaultFolder(event: Event): Promise<void> {
+    event.stopPropagation();
+    try {
+      if (this.electronService.isElectron) {
+        console.log('try select folder');
+        const result = await this.electronService.ipcRenderer.invoke('file-operations', {
+          operation: 'select-folder',
+          payload: this.defaultPath
+        });
+        if (result && result.length > 0) {
+          this.defaultPath = result[0];
+          this.defaultPathText = this.formatPath(result[0]);
+          await this.electronService.ipcRenderer.invoke('file-operations', {
+            operation: 'save-default-path',
+            payload: result[0]
+          });
+          console.log('selected folder:', result[0]);
+        }
+      }
+    } catch (error) {
+      console.error('select folder fail:', error);
+    }
+  }
 
   Images_ROC_Shown: boolean = false;
   Images_TFT_Shown: boolean = false; 
@@ -28,10 +98,6 @@ export class HomeComponent implements OnInit {
   optimize: boolean = true;
   forcelang: boolean = false;
   installEvent: string = 'install'
-
-  ngOnInit(): void {
-    console.log('HomeComponent INIT');
-  }
 
   @HostListener('mouseenter', ['$event', '$event.target.dataset.action'])
   onMouseEnter(event: MouseEvent, action: string) {
@@ -56,7 +122,6 @@ export class HomeComponent implements OnInit {
     };
   }
 
-
   @HostListener('mouseout', ['$event', '$event.target.dataset.action'])
   onMouseLeave(event: MouseEvent, action: string) {
     if (this.isInteractive) { 
@@ -79,8 +144,6 @@ export class HomeComponent implements OnInit {
       }
     };
   }
-  
-
 
   @HostListener('click', ['$event', '$event.target.dataset.action'])
   onClick(event: MouseEvent, action: string) {
@@ -89,6 +152,9 @@ export class HomeComponent implements OnInit {
         case 'Roc':
           if (!this.ROCInstall) {
             this.message = `install${this.modeState}${this.bjState}-ROC`;
+            this.Images_ROC_Shown = true;
+            this.Images_TFT_Shown = false;
+            this.Images_REF_Shown = false;
             this.TFTInstall = false;
             this.REFInstall = false;
             this.ROCInstall = !this.ROCInstall;
@@ -99,6 +165,9 @@ export class HomeComponent implements OnInit {
         case 'Tft':
           if (!this.TFTInstall) {
             this.message = `install${this.modeState}${this.bjState}-TFT`;
+            this.Images_ROC_Shown = false;
+            this.Images_TFT_Shown = true;
+            this.Images_REF_Shown = false;
             this.ROCInstall = false;
             this.REFInstall = false;
             this.TFTInstall = !this.TFTInstall;
@@ -109,6 +178,9 @@ export class HomeComponent implements OnInit {
         case 'Ref':
           if (!this.REFInstall) {
             this.message = `install${this.modeState}${this.bjState}`;
+            this.Images_ROC_Shown = false;
+            this.Images_TFT_Shown = false;
+            this.Images_REF_Shown = true;
             this.ROCInstall = false;
             this.TFTInstall = false;
             this.REFInstall = !this.REFInstall;
@@ -135,20 +207,20 @@ export class HomeComponent implements OnInit {
           console.log('mode',this.modeState,this.Mode_State);
           break;
         case 'BJoptionOn':
-            this.bjState = '';
-            this.BJ_State = 1;
-            console.log('BJ',this.bjState);
-            break;
+          this.bjState = '';
+          this.BJ_State = 1;
+          console.log('BJ',this.bjState);
+          break;
         case 'BJoptionVsAI':
-            this.bjState = '-vai';
-            this.BJ_State = 2;
-            console.log('BJ',this.bjState);
-            break;
+          this.bjState = '-vai';
+          this.BJ_State = 2;
+          console.log('BJ',this.bjState);
+          break;
         case 'BJoptionOff':
-            this.bjState = '-noc';
-            this.BJ_State = 0;
-            console.log('BJ',this.bjState);
-            break;
+          this.bjState = '-noc';
+          this.BJ_State = 0;
+          console.log('BJ',this.bjState);
+          break;
         case 'Optimise':
           this.optimize = !this.optimize;
           if (this.optimize) {
@@ -160,13 +232,8 @@ export class HomeComponent implements OnInit {
           if (this.forcelang) {
             this.optimize = false;
           }
-          break;      
+          break;     
       }
     };
   }
-
-  constructor(
-    private router: Router,
-    private electronService: ElectronService,
-  ) { }
 }
